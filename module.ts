fecd7e6c012db487d68cef96ef25cdf16e921a16
IronCPlusPlus <bsmithcompsci@gmail.com>
2021-05-30 23:48:18 -0700

    Fixed 5xx error; and better error handling.

--- module.ts
+++ module.ts
@@ -2,7 +2,8 @@ import { AttachmentAppIntegration, Module, ModuleManager } from "../../API/Modul
 import fs from "fs";
 import path from "path";
 import { NextFunction, Request, Response, Application } from 'express';
-import { HttpException } from "../../API/Routing/Routing";
+import { HttpException, Route } from "../../API/Routing/Routing";
+import { Logger } from "../../API/Common/Logging";
 
 class BaseModule extends Module
 {
@@ -24,23 +25,25 @@ class BaseModule extends Module
 
                 if (err.status == 404)
                 {
-                    // respond with json
-                    if (req.accepts('json')) 
+                    if (req.accepts('html'))
                     {
-                        return res.json({ error: 'Not found' });
+                        return Route.SafeRender(res, 'views/404', { url: req.url, status: err.status, error: err.message });
                     }
 
-                    return res.render('views/404', { url: req.url, status: err.status, error: err.message });
+                    return res.json({ url: req.url, status: err.status, error: err.message });
                 }
                 else
                 {
-                    // respond with json
-                    if (req.accepts('json')) 
+                    if (req.accepts('html'))
                     {
-                        return res.json({ url: req.url, error: err.stack, error_msg: err.message, status: res.statusCode });
+                        try {
+                            return Route.SafeRender(res, 'views/5xx', { url: req.url, error: err.stack, error_msg: err.message, status: res.statusCode });
+                        } catch (error) {
+                            Logger.error("Fatal Error with the Error Catcher ::>",error);
+                        }
                     }
-                                         
-                    return res.render('views/5xx', { url: req.url, error: err.stack, error_msg: err.message, status: res.statusCode });
+
+                    return res.json({ url: req.url, error: err.stack, error_msg: err.message, status: res.statusCode });
                 }
             });
         }, AttachmentAppIntegration.POST);
